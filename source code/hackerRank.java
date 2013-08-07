31831a0f1a945999609c0ea98385cefc21297e9c
theAJFM <febryanmargali90@gmail.com>
2013-08-07 18:39:50 +0700

    hackerRank problem

---
+++ source code/hackerRank.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class hackerRank {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner in = new Scanner(System.in);
+		int testcase = in.nextInt();
+		String b = in.nextLine();
+		String[] fill = new String[testcase];
+		for (int i = 0;i<fill.length; i++){
+			fill[i] = in.nextLine();
+			if(fill[i].matches("hackerrank") == true){
+				System.out.println(0);
+			}
+			else if(fill[i].matches(".+hackerrank$") == true){
+				System.out.println(2);
+			}
+			else if(fill[i].matches("^hackerrank.+") == true){
+				System.out.println(1);
+			}
+			else{
+				System.out.println(-1);
+			}
+		}
+    }
+}
