b02d6d4a1fcb7280989e0ade86fb906e5d58fca7
theAJFM <febryanmargali90@gmail.com>
2013-08-07 18:25:47 +0700

    Update tweet.java

--- source code/tweet.java
+++ source code/tweet.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class tweet {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 		Scanner in = new Scanner(System.in);
         int count = 0;
 		int testcase = in.nextInt();
