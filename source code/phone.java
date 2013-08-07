7abb7b64f2a058d5787adbec0bdb08351f5bba57
theAJFM <febryanmargali90@gmail.com>
2013-08-07 18:25:05 +0700

    Update phone.java

--- source code/phone.java
+++ source code/phone.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class phone {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 		Scanner in = new Scanner(System.in);
 		String telformat = "([0-9]+)-([0-9]+)-([0-9]+)";
 		String other = "([0-9]+) ([0-9]+) ([0-9]+)";
