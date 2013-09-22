c253aaeb09db1eb45cddd9c4b3b6f8c689028b33
Febryan Margali <febryanmargali90@gmail.com>
2013-09-22 15:24:25 +0700

    Update stackexchangeScraper.java

--- source code/stackexchangeScraper.java
+++ source code/stackexchangeScraper.java
@@ -14,19 +14,23 @@ public class stackexchangeScraper {
 		Pattern pattern2 = Pattern.compile(format2);
 		ArrayList<String>ID = new ArrayList<String>();
 		ArrayList<String>question = new ArrayList<String>();
+        	ArrayList<String>time = new ArrayList<String>();
 		while(in.hasNext()){
 			String assessed = in.nextLine();
 			Matcher match = pattern1.matcher(assessed);
 			Matcher match2 = pattern2.matcher(assessed);
-			while(match.find() || match2.find()){
+			while(match.find()){
 				match.groupCount();
 				ID.add(match.group(1));
 				question.add(match.group(2));
-				System.out.println("Found");
-		}
+            		}
+            		while(match2.find()){
+                		match2.groupCount();
+                		time.add(match2.group(1));
+            		}
 		}
 		for(int j = 0;j<ID.size();j++){
-			System.out.println(ID.get(j) + ";"+question.get(j));
+			System.out.println(ID.get(j) + ";"+question.get(j)+";" + time.get(j));
 		}
     }
 }
