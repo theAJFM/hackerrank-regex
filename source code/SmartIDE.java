a54d967a7e2cedc4f67255f7206fa8d086de23d7
theAJFM <febryanmargali90@gmail.com>
2013-10-04 14:31:08 +0700

    Added SmartIDE

---
+++ source code/SmartIDE.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SmartIDE {
+
+    public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedInputStream(System.in));
+		String java = ".*import java\\..*";
+		String c = ".*#include<.*";
+        boolean checked = false;
+		while(in.hasNext()){
+			String assessed = in.nextLine();
+            if(assessed.matches(java)){
+                System.out.println("Java");
+                checked = true;
+                break;
+            }
+            else if(assessed.matches(c)){
+                System.out.println("C");
+                checked = true;
+                break;
+            }
+		}
+        if(checked == false){
+            System.out.println("Python");
+        }
+    }
+}
